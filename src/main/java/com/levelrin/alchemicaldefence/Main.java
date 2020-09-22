8c57561383c553eb82afdd2e38463a066594ec6a
Rin <levelrin@gmail.com>
2020-09-23 04:31:29 +0900

    Initialize java project.

---
+++ src/main/java/com/levelrin/alchemicaldefence/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Rin (https://www.levelrin.com)
+ *
+ * This file has been created under the terms of the MIT License.
+ * See the details at https://github.com/levelrin/AlchemicalDefence/blob/master/LICENSE
+ */
+
+package com.levelrin.alchemicaldefence;
+
+/**
+ * Entrypoint of the software.
+ * It has the main method.
+ */
+@SuppressWarnings({"HideUtilityClassConstructor", "PMD.UseUtilityClass"})
+public final class Main {
+
+    /**
+     * Main method.
+     * @param args Command line arguments.
+     */
+    public static void main(final String... args) {
+        // todo: implement server side code.
+    }
+
+}
---
+++ src/main/java/com/levelrin/alchemicaldefence/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2020 Rin (https://www.levelrin.com)
+ *
+ * This file has been created under the terms of the MIT License.
+ * See the details at https://github.com/levelrin/AlchemicalDefence/blob/master/LICENSE
+ */
+
+/**
+ * Root package of the software.
+ */
+package com.levelrin.alchemicaldefence;
